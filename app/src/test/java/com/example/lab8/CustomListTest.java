5e987950677db5a77f1c3dea1a96484cdf06f569
jancisd <jancisyu@ualberta.ca>
2024-10-23 15:58:26 -0600

    Updated for Lab8

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,17 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
 
+    @Before
+    public void setUp() {
+        list = new CustomList(null, new ArrayList<>());
+    }
+
+    @Test
+    public void addCityTest() {
+        int initialSize = list.getCount();  // Get initial size of the list
+        list.addCity(new City("Toronto", "ON"));  // Add a new city
+        assertEquals(initialSize + 1, list.getCount());  // Expect size to increase by 1
+    }
 }
